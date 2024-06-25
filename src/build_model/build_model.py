3d9ab20c271d75f06ea895027031f0adba8b828d
Lee Dong Ho <70695765+remoen@users.noreply.github.com>
2024-06-25 20:53:46 +0900

    Update build_model.py

--- src/build_model/build_model.py
+++ src/build_model/build_model.py
@@ -24,22 +24,14 @@ def build_model(input_shape):
         Activation('relu'),
         MaxPooling2D((2, 2)),
         Dropout(0.3),
-
-        Conv2D(128, kernel_size=(3, 3), padding='same', kernel_regularizer=l2(0.001)),
-        BatchNormalization(),
-        Activation('relu'),
-        MaxPooling2D((2, 2)),
-        Dropout(0.3),
-
-        Reshape((-1, 128 * 16)),
-
-        LSTM(128, return_sequences=True),
-        Dropout(0.3),
+        
+        model.add(TimeDistributed(Flatten()))
+        model.add(Reshape((64, -1)))
 
         LSTM(64, return_sequences=True),
         Dropout(0.3),
 
-        LSTM(32),
+        LSTM(32, return_sequences=True),
         Dropout(0.3),
 
         Dense(2, activation='softmax')
